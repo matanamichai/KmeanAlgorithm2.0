56ae2e5812eacc40c4b363b1acde9316e2bcea89
ShahafLevin <shahaf.levinson@gmail.com>
2023-01-11 15:24:42 +0200

    working example for first input

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -1,30 +1,44 @@
 import pandas as pd
 import sys
 import numpy as np
-#import mykmeanssp as kmeans
+import mykmeanssp as kmeans
 
 def main():
-    first_df = pd.read_csv("tests/input_1_db_1.txt",header=None)
-    second_df = pd.read_csv("tests/input_1_db_2.txt",header=None)
-    merged_df = pd.merge(first_df, second_df,on=0)
-    #print(merged_df)
-    #points_array = merged_df.values.to_numpy()
-    index = np.random.choice(merged_df.index,1)
-    print(index)
-    centroid_array = merged_df.iloc[index].to_numpy()
-    print(centroid_array)
-    merged_df_distance = merged_df.copy()
-    while len(centroid_array)<5:
-        merged_df_distance["distance"] = merged_df.apply(lambda row: min(np.linalg.norm(i - row) for i in centroid_array), axis=1) # 2
-        merged_df_distance["ratio"] = merged_df_distance["distance"] / sum(merged_df_distance["distance"])  
-        #print(merged_df)              # 3
-        print("------")
-        index = np.random.choice(merged_df.index,1, p=merged_df_distance["ratio"])
-        centroid_array = np.append(centroid_array ,merged_df.iloc[index].to_numpy(),axis=0)
-        print(centroid_array)
+    first_df = pd.read_csv("tests/input_1_db_1.txt", index_col=0, header=None)
+    second_df = pd.read_csv("tests/input_1_db_2.txt", index_col=0, header=None)
+    merged_df = pd.merge(first_df, second_df, left_index=True, right_index=True) 
 
-    
-    #print(kmeans.fit(2, 2, 0.01, points_array.tolist()))
+    merged_df_initial_size = len(merged_df.index)
+    np.random.seed(0)
+
+    merged_df = merged_df.sort_index()
+
+    index = np.random.choice(merged_df.index, 1)
+    print(f'chosen index is: {index}')
+
+    centroid_array = merged_df.iloc[index].to_numpy()
+    print(f'centroids array:\n{centroid_array}')
+    merged_df = merged_df.drop(index)
+
+    while len(centroid_array) < 3:
+        merged_df["distance"] = merged_df.apply(lambda row: min(np.linalg.norm(i - row) for i in centroid_array), axis=1)
+        merged_df["ratio"] = merged_df["distance"] / sum(merged_df["distance"])  
+        print("-" * 10)
+
+        index = np.random.choice(merged_df.index, 1, p=merged_df["ratio"])
+        print(f"index: {index}")
+        row_to_append = merged_df.loc[:, ~merged_df.columns.isin(["distance", "ratio"])].loc[index].to_numpy()
+
+        centroid_array = np.append(centroid_array, row_to_append, axis=0)
+
+        merged_df = merged_df.drop(index)
+        merged_df = merged_df.drop(["distance", "ratio"], axis=1)
+
+        print(f'updated:\n{centroid_array}')
+
+    arr = centroid_array.tolist() + merged_df.to_numpy().tolist()
+    assert(len(arr) == merged_df_initial_size)
+    print(kmeans.fit(3, 33, 0, arr))
 
 
 if __name__ == "__main__":
